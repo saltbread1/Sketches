bdc4eb6ec9d558218b70e6c2670b98dc987b536e
saltbread1 <takenokonosato913@icloud.com>
2025-06-22 18:23:41 +0900

    utils

--- src/main/kotlin/util/Util.kt
+++ src/main/kotlin/util/Util.kt
@@ -1,12 +1,24 @@
 package util
 
+import java.io.File
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.reflect.KClass
 
 fun timestamp(format: String): String
 {
     val now = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"))
     val formatter = DateTimeFormatter.ofPattern(format)
     return now.format(formatter)
+}
+
+fun createPalette(colors: String): IntArray
+{
+    return colors.split("-").map { it.toInt(16) }.toIntArray()
+}
+
+fun saveName(clazz: KClass<*>): String
+{
+    return "output" + File.separator + clazz.simpleName + File.separator + timestamp("yyyyMMddHHmmss") + "-######.png"
 }
