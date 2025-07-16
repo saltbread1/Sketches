a26fff86c7f48774749f8c108ec5ebb30df77f82
saltbread1 <takenokonosato913@icloud.com>
2025-07-16 16:32:53 +0900

    add comment

--- src/main/kotlin/mesh/Vertex.kt
+++ src/main/kotlin/mesh/Vertex.kt
@@ -2,4 +2,10 @@ package mesh
 
 import processing.core.PVector
 
+/**
+ * Represents a vertex in a half-edge mesh structure.
+ *
+ * @property position The position of the vertex in 3D space.
+ * @property outgoing A reference to one of the outgoing half-edges originating from this vertex.
+ */
 data class Vertex(var position: PVector, var outgoing: HalfEdge?)
