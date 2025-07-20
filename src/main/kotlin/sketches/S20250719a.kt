89083cc4bc2395fa781a03e9613d72166f1656cf
saltbread1 <takenokonosato913@icloud.com>
2025-07-20 18:53:32 +0900

    projection

--- src/main/kotlin/sketches/S20250719a.kt
+++ src/main/kotlin/sketches/S20250719a.kt
@@ -7,22 +7,19 @@ import processing.core.PVector
 class S20250719a : ExtendedPApplet(P3D)
 {
     private val palette = createPalette("001219-005f73-0a9396-94d2bd-e9d8a6-ee9b00-ca6702-bb3e03-ae2012-9b2226")
-    private val eye = PVector(0.0f, 0.0f, 1.5f)
+    private val eye = PVector(0.0f, 0.0f, 2.0f)
     private val center = PVector(0.0f, 0.0f, 0.0f)
-    private val fov = HALF_PI
-    private val far = 10.0f
     private val meshData = Icosahedron()
     private val mesh = HalfEdgeMesh()
-    private val pentagons = mutableListOf<Pentagon>()
-    private val remainFaces = mutableListOf<Int>()
+    private val polygons = mutableListOf<FractalPolygon>()
+    private val viewFactor = 2.0f
 
     override fun setup()
     {
         mesh.buildMesh(meshData)
-        mesh.subdivide { v0, v1 -> PVector.lerp(v0, v1, 0.5f) }
-        mesh.subdivide { v0, v1 -> PVector.lerp(v0, v1, 0.5f) }
+        mesh.subdivide { v0, v1 -> PVector.lerp(v0, v1, 0.5f).normalize() }
         val parentNumVertices = MutableList(mesh.getVertexCount()) { it }
-        mesh.subdivide { v0, v1 -> PVector.lerp(v0, v1, 0.5f) }
+        mesh.subdivide { v0, v1 -> PVector.lerp(v0, v1, 0.5f).normalize() }
 
         val error = mesh.validate()
         if (error.isNotEmpty())
@@ -30,42 +27,53 @@ class S20250719a : ExtendedPApplet(P3D)
             throw IllegalStateException("Invalid mesh state: $error")
         }
 
-        pentagons.clear()
-        remainFaces.clear()
-        val initPentagons = mutableListOf<Pentagon>()
+        val remainFaces = mutableListOf<Int>()
+        val initPolygons = mutableListOf<FractalPolygon>()
 
         parentNumVertices.forEach { v ->
             val adjacent = mesh.getAdjacentVertices(v).reversed()
-            initPentagons.add(Pentagon(
+            initPolygons.add(FractalPolygon(
                 adjacent.map { Attribute(
                     mesh.getVertexPosition(it) ?: return@forEach,
-                    mesh.getVertexNormal(it) ?: return@forEach
+                    mesh.getVertexNormal(it) ?: return@forEach,
                 ) },
                 Attribute(
                     mesh.getVertexPosition(v) ?: return@forEach,
-                    mesh.getVertexNormal(v) ?: return@forEach
+                    mesh.getVertexNormal(v) ?: return@forEach,
                 ),
-                ))
+            ))
         }
 
         // remain triangle polygons
         repeat(mesh.getFaceCount()) { remainFaces.add(it) }
         remainFaces.removeAll(parentNumVertices.map { mesh.getAdjacentFaces(it) }.flatten())
-
-        // division pentagon
-        var tmpPentagons = initPentagons.toList()
-        repeat(3)
-        {
-            tmpPentagons = tmpPentagons.flatMap { it.subdivision() }
+        remainFaces.forEach { face ->
+            val vertices = mesh.getFaceVertices(face)
+            initPolygons.add(FractalPolygon(
+                vertices.map { Attribute(
+                    mesh.getVertexPosition(it) ?: return@forEach,
+                    mesh.getVertexNormal(it) ?: return@forEach,
+                ) },
+                Attribute(
+                    vertices.map { mesh.getVertexPosition(it) }.reduce { acc, v -> PVector.add(acc, v) }?.normalize() ?: return@forEach,
+                    vertices.map { mesh.getVertexNormal(it) }.reduce { acc, v -> PVector.add(acc, v) }?.normalize() ?: return@forEach,
+                ),
+            ))
         }
 
-        // pentagon meshes
-        pentagons.addAll(tmpPentagons)
+        // division pentagon
+        var tmpPentagons = initPolygons.toList()
+        repeat(3) { tmpPentagons = tmpPentagons.flatMap { it.subdivision() } }
 
-        perspective(fov, aspect, 0.1f, far)
+        // pentagon meshes
+        polygons.clear()
+        polygons.addAll(tmpPentagons)
+
+//        perspective(PI * 0.3f, aspect, 0.1f, 10.0f)
+        ortho(-aspect * viewFactor, aspect * viewFactor, -viewFactor, viewFactor, 0.0f, 10.0f)
         camera(eye.x, eye.y, eye.z, center.x, center.y, center.z, 0.0f, 1.0f, 0.0f)
 
-//        noLoop()
+        noLoop()
     }
 
     override fun draw()
@@ -73,37 +81,42 @@ class S20250719a : ExtendedPApplet(P3D)
         background(12.0f, 12.0f, 18.0f)
 
 //        val lightPos = PVector(3.0f, -6.0f, 3.0f)
-        val lightPos = PVector(0.0f, 0.0f, eye.z)
-        val ambient = PVector(0.1f, 0.1f, 0.1f).mult(255.0f)
-        val diffuse = PVector(0.8f, 0.8f, 0.8f).mult(255.0f)
+//        val lightPos = PVector(0.0f, 0.0f, eye.z)
+//        val ambient = PVector(12.0f, 12.0f, 18.0f)
+//        val diffuse = PVector(180.0f, 180.0f, 200.0f)
 
-        push()
+        pushStyle()
         noStroke()
 //        stroke(0.0f)
 
-        pointLight(diffuse.x, diffuse.y, diffuse.z, lightPos.x, lightPos.y, lightPos.z)
-        ambientLight(ambient.x, ambient.y, ambient.z)
-        specular(255.0f)
-        shininess(60.0f)
+//        pointLight(diffuse.x, diffuse.y, diffuse.z, lightPos.x, lightPos.y, lightPos.z)
+//        ambientLight(ambient.x, ambient.y, ambient.z)
+//        specular(255.0f)
+//        shininess(60.0f)
 
-        rotateY(frameCount * 0.005f)
+//        polygons.forEach { it.draw() }
 
-        pentagons.forEach { it.draw() }
-
-        fill(255.0f)
-        remainFaces.forEach { face ->
-            val vertices = mesh.getFaceVertices(face)
-            beginShape()
-            vertices.forEach inner@ { v ->
-                val pos = mesh.getVertexPosition(v) ?: return@inner
-                val nor = mesh.getVertexNormal(v) ?: return@inner
-                normal(nor.x, nor.y, nor.z)
-                vertex(pos.x, pos.y, pos.z)
-            }
+        polygons.forEach { poly ->
+            val projVertPos = poly.vertices.map { project(it.position) }
+            val projCenter = project(poly.center.position)
+            if (projVertPos.none { it.x >= -aspect * viewFactor && it.x <= aspect * viewFactor && it.y >= -viewFactor && it.y <= viewFactor }) return@forEach
+            fill(poly.color)
+            beginShape(TRIANGLE_FAN)
+            vertex(projCenter.x, projCenter.y)
+            projVertPos.forEach { vertex(it.x, it.y) }
+            vertex(projVertPos.first().x, projVertPos.first().y)
             endShape()
         }
 
-        pop()
+        popStyle()
+    }
+
+    private fun customRandom() = sigmoid(randomGaussian(), 1.2f)
+
+    private fun project(pos: PVector): PVector
+    {
+        val k = 1.0f - signOrZero(pos.z) * sqrt(1.0f - pos.x * pos.x - pos.y * pos.y).let { if (it.isFinite()) it else 1.0f }
+        return PVector(pos.x / k, pos.y / k)
     }
 
     private inner class Attribute(val position: PVector, val normal: PVector)
@@ -114,25 +127,20 @@ class S20250719a : ExtendedPApplet(P3D)
             vertex(position.x, position.y, position.z)
         }
 
-        fun add(attrib: Attribute) = Attribute(
-            PVector.add(position, attrib.position),
-            PVector.add(normal, attrib.normal).normalize(),
-        )
-
         fun lerp(attrib: Attribute, amt: Float) = Attribute(
-            PVector.lerp(position, attrib.position, amt),
+            PVector.lerp(position, attrib.position, amt).normalize(),
             PVector.lerp(normal, attrib.normal, amt).normalize(),
         )
     }
 
-    private inner class Pentagon(
-        private val vertices: List<Attribute>,
-        private val center: Attribute,
+    private inner class FractalPolygon(
+        val vertices: List<Attribute>,
+        val center: Attribute,
     )
     {
-        private val color = palette.random()
+        val color = palette.random()
 
-        fun subdivision() : List<Pentagon>
+        fun subdivision(): List<FractalPolygon>
         {
             val middles = vertices.mapIndexed { i, v0 ->
                 val v1 = vertices[(i + 1) % vertices.size]
@@ -140,18 +148,29 @@ class S20250719a : ExtendedPApplet(P3D)
             }
 
             val centerVertices = middles.map { mid ->
-                mid.lerp(center, 0.5f)
+                mid.lerp(center, customRandom())
             }
 
             val ret = vertices.mapIndexed { i, _ ->
                 val i2 = (i + 1) % vertices.size
                 val newVertices = listOf(vertices[i2], middles[i2], centerVertices[i2], centerVertices[i], middles[i])
-                val newCenter = newVertices.reduce { acc, v -> acc.add(v) }
-                newCenter.position.div(newVertices.size.toFloat())
-                Pentagon(newVertices, newCenter)
+                FractalPolygon(newVertices, calcCenter(newVertices))
             }.toMutableList()
 
-            ret.add(Pentagon(centerVertices, center))
+            ret.add(FractalPolygon(centerVertices, center))
+
+            return ret
+        }
+
+        private fun calcCenter(points: List<Attribute>): Attribute
+        {
+            val ret = points.reduce { acc, v -> Attribute(
+                    PVector.add(acc.position, v.position),
+                    PVector.add(acc.normal, v.normal),
+                )
+            }
+            ret.position.normalize()
+            ret.normal.normalize()
 
             return ret
         }
@@ -160,14 +179,11 @@ class S20250719a : ExtendedPApplet(P3D)
         {
             pushStyle()
             fill(color)
-            vertices.forEachIndexed { i, attrib0 ->
-                val attrib1 = vertices[(i + 1) % vertices.size]
-                beginShape()
-                center.vertex()
-                attrib0.vertex()
-                attrib1.vertex()
-                endShape()
-            }
+            beginShape(TRIANGLE_FAN)
+            center.vertex()
+            vertices.forEach { v -> v.vertex() }
+            vertices.first().vertex()
+            endShape()
             popStyle()
         }
     }
