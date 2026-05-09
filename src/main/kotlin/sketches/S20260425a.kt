89d18d00585f87c60c2137358292563cc7593f04
saltbread1 <takenokonosato913@icloud.com>
2026-05-10 01:40:11 +0900

    260425a

--- src/main/kotlin/sketches/S20260425a.kt
+++ src/main/kotlin/sketches/S20260425a.kt
@@ -5,7 +5,7 @@ import util.Quaternion
 
 
 @Suppress("unused")
-class S20260425a : ExtendedPApplet(P3D, false)
+class S20260425a : ExtendedPApplet(P3D)
 {
     private val fov = PI * 0.5f
     private val far = 10.0f
@@ -13,15 +13,17 @@ class S20260425a : ExtendedPApplet(P3D, false)
         resourcePath("shaders/fog.frag")!!,
         resourcePath("shaders/fog.vert")!!,
     ) }
-    private val fogColor = 0xff121216.toInt()
+    private val fogColor = PAPER_WHITE
 
     override fun setup()
     {
         perspective(fov, aspect, 0.1f, far)
-        camera(0.0f, 0.0f, 0.0f, 0.0f, -0.5f, -2.0f, 0.0f, 1.0f, 0.0f)
+        camera(0.0f, 0.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f)
 
-        fogShader.set("fogRange", 2.0f, 5.0f)
+        fogShader.set("fogRange", 3.0f, 5.0f)
         fogShader.set("fogColor", red(fogColor) / 255.0f, green(fogColor) / 255.0f, blue(fogColor) / 255.0f)
+
+        colorMode(HSB, 1.0f, 1.0f, 1.0f, 1.0f)
 
         noLoop()
     }
@@ -30,26 +32,48 @@ class S20260425a : ExtendedPApplet(P3D, false)
     {
         background(fogColor)
 
-        val seed = random(Int.MAX_VALUE.toFloat())
-
         shader(fogShader)
         repeat(200) {
             val ymax = tan(fov * 0.5f)
-            val u = random(0.0f, 1.0f)
-            val x = sign(u - 0.5f) * pow(abs(2.0f * u - 1.0f), 2.6f)
-            var y = randomGaussian()
-            while (y !in -1.0f .. 1.0f)
-            {
-                y = randomGaussian()
-            }
+            val x = random(-2.0f, 2.0f)
+            val y = noise(x * 8.0f, frameCount.toFloat()) * 2.0f - 1.0f + randomGaussian() * 0.2f
             val cdir = PVector(
                 x * ymax * aspect,
-                lerp(y, noise(x * 16.0f, seed) * 2.0f - 1.0f, 0.6f) * 0.5f * ymax,
-                -random(1.0f),
+                y * ymax,
+                -1.0f,
             ).normalize()
             val dir = viewToWorld(cdir).normalize()
-            val prism = CircularPrism(0.3f, 4.0f, PVector.mult(dir, 5.0f))
-            prism.draw(2000)
+            val baseHue = random(8.0f).toInt() / 8.0f
+            val prism = CircularPrism(
+                random(0.25f, 0.45f),
+                random(3.0f, 4.0f),
+                PVector.mult(dir, 5.0f),
+                random(1.0f, 2.5f),
+                random(1.0f, 4.0f) * TAU) {
+                    color(baseHue + random(0.16f), 1.0f, 1.0f)
+            }
+            prism.draw(4000)
+        }
+        repeat(1000) {
+            val ymax = tan(fov * 0.5f)
+            val x = random(-2.0f, 2.0f)
+            val y = random(-2.0f, 2.0f)
+            val cdir = PVector(
+                x * ymax * aspect,
+                y * ymax,
+                -1.0f,
+            ).normalize()
+            val dir = viewToWorld(cdir).normalize()
+            val baseHue = random(8.0f).toInt() / 8.0f
+            val prism = CircularPrism(
+                random(0.2f, 0.3f),
+                random(2.0f, 3.0f),
+                PVector.mult(dir, 5.0f),
+                random(1.0f, 2.5f),
+                random(1.0f, 2.0f) * TAU) {
+                color(baseHue + random(0.08f), 0.8f, 0.6f)
+            }
+            prism.draw(500)
         }
         resetShader()
 
@@ -67,12 +91,16 @@ class S20260425a : ExtendedPApplet(P3D, false)
 
     data class Circle(val extent: Float, val center: PVector, val color: Int)
 
-    inner class CircularPrism(val radius: Float, val height: Float, val bottomCenter: PVector)
+    inner class CircularPrism(
+        val radius: Float,
+        val height: Float,
+        val bottomCenter: PVector,
+        val maxROff: Float,
+        val maxPhiOff: Float,
+        val colorGetter: () -> Int)
     {
         val seed = random(Int.MAX_VALUE.toFloat())
-        val maxROff = random(1.0f, 2.5f)
-        val noiseScale = random(2.0f, 4.0f)
-        val col = color(random(4.0f, 12.0f).toInt() / 20.0f * 255.0f)
+        val noiseScale = random(2.0f, 16.0f)
 
         fun randomPosition(): Circle
         {
@@ -81,7 +109,7 @@ class S20260425a : ExtendedPApplet(P3D, false)
             val r = radius * (1.0f - sq(z0))
             val phi = random(0.0f, TAU)
             val rOff = maxROff * radius * noise(z0 * noiseScale, seed)
-            val phiOff = TAU * noise(z0 * noiseScale, seed + 100.0f)
+            val phiOff = maxPhiOff * noise(z0 * noiseScale, seed + 100.0f)
             val x = r * sin(phi) + rOff * sin(phiOff)
             val z = r * cos(phi) + rOff * cos(phiOff)
 
@@ -91,7 +119,8 @@ class S20260425a : ExtendedPApplet(P3D, false)
 
             val pos =  rotate(qua, PVector(x, y, z)).add(bottomCenter)
             val ext = radius * (0.1f + sqrt(1.0f - sq(z0))) * 0.2f
-            return Circle(ext, pos, col)
+            val extOff = sq(random(1.0f)) * ext
+            return Circle(ext + extOff, pos, colorGetter.invoke())
         }
 
         fun draw(count: Int)
